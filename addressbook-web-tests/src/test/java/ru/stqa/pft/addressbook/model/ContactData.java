cf83c7d1beab383b106710206d60953da0af2bd2
nataly2304 <chistova.nataliia@yandex.ru>
2019-07-12 16:01:42 +0300

    modification ContactDeletionTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -34,4 +34,26 @@ public class ContactData {
   public String getGroup() {
     return group;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    ContactData that = (ContactData) o;
+
+    return firstname != null ? firstname.equals(that.firstname) : that.firstname == null;
+  }
+
+  @Override
+  public int hashCode() {
+    return firstname != null ? firstname.hashCode() : 0;
+  }
+
+  @Override
+  public String toString() {
+    return "ContactData{" +
+            "firstname='" + firstname + '\'' +
+            '}';
+  }
 }
